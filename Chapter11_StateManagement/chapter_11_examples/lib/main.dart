87b205e419120f0902ec7ce8d07cae06e7179b5d
Nico Krieg <n.krieg123@googlemail.com>
2021-02-04 21:52:23 +0100

    Add some notes on best practices for state management.

--- Chapter11_StateManagement/chapter_11_examples/lib/main.dart
+++ Chapter11_StateManagement/chapter_11_examples/lib/main.dart
@@ -12,6 +12,26 @@ import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
+// Short summary of best practices about state management...
+// ... using Provider
+// * Use 'Consumer<T>' wherever possible because it enabled rebuilding only the
+// widgets that actually require a rebuild.
+// * 'Provider.of<T>(context)' should be avoided inside 'build()' methods as it will
+// all of the widget's children, even those that don't need to be rebuilt.
+// * Making use of 'MultiProvider' is a good means to make code more readable in
+// case multiple providers are to be used within and below the same widget.
+// * Prefer using extensions methods introduced by Provider to the 'BuildContext'
+// class (for example: 'context.watch<T>()') over the more elaborate static methods
+// on the Provider class.
+// ... using BLoC
+// * Use 'context.bloc<T>()' rather than 'BlocProvider.of<T>(context)' (so, again,
+// prefer using extension methods)
+// * A 'BlocBuilder<T>()' should contain only the widgets that actually need to
+// rebuild in order to respond to some change.
+// * Make use of 'BlocConsumer<B, S>' instead of nesting 'BlocBuilder' and
+// 'BlocListener'.
+// * 'ReplayBloc' or 'ReplayBlocMixin' are a great option to introduce undo and
+// redo actions to the app's blocs.
 Future<void> main() async {
   // The BlocObserver is very useful in cases where things happening within
   // Blocs need to be logged or analyzed. The BlocObserver passively observes; it
@@ -86,6 +106,15 @@ class BlocWithListenerHome extends StatelessWidget {
   }
 }
 
+// Some best practices for using BLoC:
+// * Nesting a 'BlocBuilder' inside a 'BlocListener' is not a problem, it's just a
+// lot of code to write. Therefore, if one needs to rebuild the UI and listen to
+// state changes, one will be better of using a 'BlocConsumer'. In all other cases,
+// making use of 'BlocBuilder' and 'BlocListener' is absolutely fine.
+// * The content of a bloc's child should be moved to a separate widget so there's
+// a change to initialize the child widget using a 'const' constructor.
+// * Use a 'MultiBlocProvider' if more than one bloc is required within the same
+// widget.
 class BlocHome extends StatelessWidget {
   const BlocHome();
 
