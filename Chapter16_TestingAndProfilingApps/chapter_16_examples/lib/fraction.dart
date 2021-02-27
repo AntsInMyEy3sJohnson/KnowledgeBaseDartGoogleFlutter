e174929be1d846908dd9d23464e92b430cc71048
Nico Krieg <n.krieg123@googlemail.com>
2021-02-27 08:59:38 +0100

    Add more unit-level tests to test suite for Fraction class.

--- Chapter16_TestingAndProfilingApps/chapter_16_examples/lib/fraction.dart
+++ Chapter16_TestingAndProfilingApps/chapter_16_examples/lib/fraction.dart
@@ -2,17 +2,23 @@ class Fraction {
   int _n;
   int _d;
 
-  Fraction({int n = 0, int d = 1})
-      : _n = n,
-        _d = d;
-  
-  void negate() => _n * -1;
-  
+  Fraction({int n = 0, int d = 1}) {
+    _n = n;
+    _d = d == 0 ? throw FractionException("Denominator must not be zero.") : d;
+  }
+
+  void negate() => _n *= -1;
+
   double get toDouble => _n / _d;
 
   @override
   String toString() {
     return 'Fraction{_n: $_n, _d: $_d}';
   }
+}
 
+class FractionException implements Exception {
+  final String message;
+
+  const FractionException(this.message);
 }
--- Chapter16_TestingAndProfilingApps/chapter_16_examples/test/fraction_unit_test.dart
+++ Chapter16_TestingAndProfilingApps/chapter_16_examples/test/fraction_unit_test.dart
@@ -2,12 +2,31 @@ import 'package:chapter_16_examples/fraction.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Expect 6 divided by 3 to be 2.', () {
-    final fraction = Fraction(n: 6, d: 3);
+  group('All testing related to the "Fraction" class.', () {
+    test('Expect 6 divided by 3 to be 2.', () {
+      final fraction = Fraction(n: 6, d: 3);
 
-    final expected = 2.0;
-    final actual = fraction.toDouble;
+      final expected = 2.0;
 
-    expect(actual, expected);
+      // Matcher argument here is only a single value, but the matcher library
+      // offers many useful methods to be used.
+      expect(fraction.toDouble, expected);
+    });
+
+    test('Expect invocation of "negate" to produce negated number.', () {
+      final fraction = Fraction(n: 10, d: 2);
+
+      final expected = -5.0;
+
+      fraction.negate();
+
+      expect(fraction.toDouble, expected);
+    });
+
+    test(
+        'Expect exception to be thrown when attempting to '
+        'initialize Fraction object with 0 denominator.', () {
+      expect(() => Fraction(n: 1, d: 0), throwsA(isA<FractionException>()));
+    });
   });
 }
