f212c305b1ab9743e7589441ba104a495620e4da
Nico Krieg <n.krieg123@googlemail.com>
2021-05-03 21:37:07 +0200

    Add notes on best practices to HTTP request code

--- 13_Networking/chapter_17_examples/lib/http_post_requests/post_http_request.dart
+++ 13_Networking/chapter_17_examples/lib/http_post_requests/post_http_request.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'dart:ffi';
 
 import 'package:chapter_17_examples/commons/http_request.dart';
 import 'package:chapter_17_examples/http_post_requests/data/post.dart';
@@ -12,6 +13,10 @@ class PostHttpRequest implements SimplePostRequest<Post> {
 
   @override
   Future<Post> perform(String body) async {
+    // When issuing only a single HTTP call, using 'http.get()' or 'http.post()'
+    // is fine, but in case multiple requests are to be made against the same
+    // server, employing an instance of 'http.Client' is preferable.
+    // See dummy method below
     final response =
         await http.post(url, body: body, encoding: Encoding.getByName("utf-8"));
 
@@ -20,5 +25,19 @@ class PostHttpRequest implements SimplePostRequest<Post> {
     }
 
     throw HttpRequestException("${response.statusCode}: ${response.body}");
+  }
+
+  /// Dummy method showing examples for how to make use of 'http.Client()'
+  void _performWithClient(String body) async {
+
+    final client = http.Client();
+
+    try {
+      final firstRequest = await client.get("https://some.url");
+      final secondRequest = await client.get("https://some-other.url");
+    } finally {
+      client.close();
+    }
+
   }
 }
--- 13_Networking/chapter_17_examples/lib/http_post_requests/routes/result_page.dart
+++ 13_Networking/chapter_17_examples/lib/http_post_requests/routes/result_page.dart
@@ -22,13 +22,20 @@ class _ResultPageState extends State<ResultPage> {
   @override
   void initState() {
     super.initState();
+    // Good place to initialize a Future<T> is always the 'initState()' method
+    // in order to prevent each rebuild from triggering whatever call the Future<T>
+    // encapsulates (here: an HTTP call towards an online API)
     _postFuture = widget._postHttpRequest.perform(widget._postAsJson);
   }
 
   @override
   Widget build(BuildContext context) {
+    // The best way to wait for a Future to complete in a Flutter app is the
+    // 'FlutterBuilder' widget
     return FutureBuilder(
         future: _postFuture,
+        // This builder function will be invoked many times, hence using a
+        // 'const' constructor as often as possible is vital
         builder: (context, snapshot) {
           if (snapshot.hasError) {
             return const Center(child: Icon(Icons.error_rounded));
